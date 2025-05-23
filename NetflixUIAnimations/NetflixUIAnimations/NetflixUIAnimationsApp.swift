7402f65ec23de6d4e85bf658e70d0f41b58561ec
Leon Salvatore <elione007@gmail.com>
2025-05-23 10:24:31 +0200

    - refactor: code change (not a feature or bug)

---
+++ NetflixUIAnimations/NetflixUIAnimations/NetflixUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixUIAnimationsApp.swift
+//  NetflixUIAnimations
+//
+//  Created by Leon Salvatore on 23.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct NetflixUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
